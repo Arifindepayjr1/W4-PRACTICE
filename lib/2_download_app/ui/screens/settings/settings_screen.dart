e68cc8f9fc112ed29c8114391a574b642cd44455
Arifin Depay Jr <121757305+Arifindepayjr1@users.noreply.github.com>
2026-02-23 04:35:41 +0700

    TASK-002-DOWNLOADER-COLOR THEME

---
+++ lib/2_download_app/ui/screens/settings/settings_screen.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import '../../providers/theme_color_provider.dart';
+import '../../theme/theme.dart';
+import 'widget/theme_color_button.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    ValueNotifier<ThemeColor> currentColorTheme = ValueNotifier<ThemeColor>(
+      currentThemeColor,
+    );
+    return ValueListenableBuilder(
+      valueListenable: currentColorTheme,
+      builder: (context, value, child) {
+        return Container(
+          color: value.backgroundColor,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 16),
+              Text(
+                "Settings",
+                style: AppTextStyles.heading.copyWith(
+                  color: currentThemeColor.color,
+                ),
+              ),
+
+              SizedBox(height: 50),
+
+              Text(
+                "Theme",
+                style: AppTextStyles.label.copyWith(color: AppColors.textLight),
+              ),
+
+              SizedBox(height: 10),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: ThemeColor.values
+                    .map(
+                      (theme) => ThemeColorButton(
+                        themeColor: theme,
+                        isSelected: theme == value,
+                        onTap: (_) {
+                          currentColorTheme.value = theme;
+                        },
+                      ),
+                    )
+                    .toList(),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
